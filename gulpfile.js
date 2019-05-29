5901a4865aa7aaa544d38004b75d1ba2d3c307c2
toshiki.kaneko <toshiki.kaneko@xrosssell.co.jp>
2019-05-29 16:51:34 +0900

    環境構築DB接続

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require("gulp");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var plumber = require("gulp-plumber");
 var browser = require("browser-sync");
 var typescript = require('gulp-typescript');
 
@@ -16,6 +20,17 @@ gulp.task('html',function(){
       .pipe(gulp.dest("www"))
 });
 
+//sass
+gulp.task('sass', function() {
+  gulp.src("src/main.scss")
+      .pipe(plumber())
+      .pipe(sass({outputStyle: 'expanded'}))
+      .pipe(autoprefixer())
+      .pipe(rename('app.css'))
+      .pipe(gulp.dest("www"))
+      .pipe(browser.reload({stream:true}))
+});
+
 // typescript
 gulp.task('ts', function() {  
   gulp.src('src/**/*.ts')
@@ -25,10 +40,11 @@ gulp.task('ts', function() {
 });
 
 //build
-gulp.task("build", ['html']);
+gulp.task("build", ['html', 'sass', 'ts']);
 
 //タスクの自動化
 gulp.task("default", ['build', 'server'], function() {
   gulp.watch("src/**/*.html", ["html"]);
+  gulp.watch("src/**/*.scss", ["sass"]);
   gulp.watch("src/**/*.ts", ["ts"]);
 });
